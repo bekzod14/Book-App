8288c15f0166a71416f11aa6206054c7ee5c56b1
jamshidisoqov <isoqovjamshid01@gmail.com>
2022-10-06 15:42:05 +0500

    --bottom nav page added

---
+++ app/src/main/java/uz/gita/bookappcompose/ui/books/BooksScreen.kt
@@ -0,0 +1,12 @@
+package uz.gita.bookappcompose.ui.books
+
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.androidx.AndroidScreen
+
+// Created by Jamshid Isoqov an 10/6/2022
+class BooksScreen : AndroidScreen() {
+    @Composable
+    override fun Content() {
+
+    }
+}
---
+++ app/src/main/java/uz/gita/bookappcompose/ui/saved/SavedBooksScreen.kt
@@ -0,0 +1,12 @@
+package uz.gita.bookappcompose.ui.saved
+
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.androidx.AndroidScreen
+
+// Created by Jamshid Isoqov an 10/6/2022
+class SavedBooksScreen :AndroidScreen() {
+    @Composable
+    override fun Content() {
+
+    }
+}
