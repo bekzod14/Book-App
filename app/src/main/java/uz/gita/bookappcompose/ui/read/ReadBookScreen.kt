a8743cbe7ce4ed889a8cffa7d000c5b0885f35d0
jamshidisoqov <isoqovjamshid01@gmail.com>
2022-10-07 08:58:34 +0500

    --pdf viewer library added

--- app/src/main/java/uz/gita/bookappcompose/ui/read/ReadBookScreen.kt
+++ app/src/main/java/uz/gita/bookappcompose/ui/read/ReadBookScreen.kt
@@ -1,5 +1,25 @@
 package uz.gita.bookappcompose.ui.read
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import cafe.adriel.voyager.androidx.AndroidScreen
+
 // Created by Jamshid Isoqov an 10/6/2022
-class ReadBookScreen {
+class ReadBookScreen : AndroidScreen() {
+    @Composable
+    override fun Content() {
+
+    }
 }
+
+@Composable
+fun ReadBookScreenContent() {
+
+}
+
+@Composable
+@Preview(showSystemUi = true)
+fun ReadBookScreenPreview() {
+
+}
+
