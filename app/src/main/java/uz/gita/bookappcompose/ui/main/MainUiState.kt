786c29b4919ea56e2875ea553c072f911e81edfb
Shahrizod <qosimovshaxrizod7376@gmail.com>
2022-10-07 15:07:51 +0500

    nimadir

--- app/src/main/java/uz/gita/bookappcompose/ui/main/MainUiState.kt
+++ app/src/main/java/uz/gita/bookappcompose/ui/main/MainUiState.kt
@@ -1,7 +1,3 @@
 package uz.gita.bookappcompose.ui.main
 
 // Created by Jamshid Isoqov an 10/6/2022
-sealed interface MainUiState {
-    object Books : MainUiState
-    object SavedBooks : MainUiState
-}
--- app/src/main/java/uz/gita/bookappcompose/ui/main/MainViewModel.kt
+++ app/src/main/java/uz/gita/bookappcompose/ui/main/MainViewModel.kt
@@ -4,3 +4,22 @@ import uz.gita.bookappcompose.utils.AppViewModel
 
 // Created by Jamshid Isoqov an 10/6/2022
 interface MainViewModel : AppViewModel<MainIntent, MainUiState, Nothing>
+
+sealed interface Intent {
+
+
+}
+
+sealed interface MainIntent {
+
+    object BooksClicked : MainIntent
+    object SavedBooksClicked : MainIntent
+
+}
+
+sealed interface MainUiState {
+
+    object Books : MainUiState
+    object SavedBooks : MainUiState
+
+}
