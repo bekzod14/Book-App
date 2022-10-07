9c7e78f8004731c07c4244ce9958edaae99dd411
Azimjon <makhmudovazimjon53@gmail.com>
2022-10-07 15:01:22 +0500

    add main use case

--- app/src/main/java/uz/gita/bookappcompose/domain/di/UseCaseModule.kt
+++ app/src/main/java/uz/gita/bookappcompose/domain/di/UseCaseModule.kt
@@ -4,7 +4,9 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import uz.gita.bookappcompose.domain.usecase.MainUseCase
 import uz.gita.bookappcompose.domain.usecase.SplashUseCase
+import uz.gita.bookappcompose.domain.usecase.impl.MainUseCaseImpl
 import uz.gita.bookappcompose.domain.usecase.impl.SplashUseCaseImpl
 
 
@@ -14,4 +16,7 @@ interface UseCaseModule {
 
     @Binds
     fun bind(splashUseCase: SplashUseCaseImpl): SplashUseCase
+
+    @Binds
+    fun bind(mainUseCase: MainUseCaseImpl): MainUseCase
 }
