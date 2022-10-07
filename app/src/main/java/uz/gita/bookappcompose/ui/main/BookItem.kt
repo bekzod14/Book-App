922745b4e27b791f40aeb3d1192520987dc93bcf
Arzigul-Developer <arzigul.nazarbaeva1@s.amity.edu>
2022-10-07 15:37:24 +0500

    -book item

--- app/src/main/java/uz/gita/bookappcompose/ui/main/BookItem.kt
+++ app/src/main/java/uz/gita/bookappcompose/ui/main/BookItem.kt
@@ -1,41 +1,92 @@
 package uz.gita.bookappcompose.ui.main
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import uz.gita.bookappcompose.R
+import uz.gita.bookappcompose.data.models.BookData
 import uz.gita.bookappcompose.ui.theme.BookAppTheme
 
+@Preview
+@Composable
+fun MockView() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        LazyColumn {
+            item {
+                BookItem(
+                    BookData(
+                        1,
+                        "Halqa",
+                        "Ajoyib",
+                        "",
+                        "",
+                        "Akrom Malik",
+                        1,
+                        "",
+                        1,
+                        1,
+                        1
+                    )
+                )
+            }
+        }
+    }
+}
 
 @Composable
 fun BookItem(
-    image: Int,
-    author: String,
-    bookName: String,
+    bookData: BookData
 ) {
-    Column {
+    Column(
+        modifier = Modifier
+            .padding(10.dp)
+            .size(200.dp, 310.dp)
+    ) {
 
-        Card {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(6f)
+        ) {
             Image(
-                painter = painterResource(id = image),
+                painter = painterResource(id = R.drawable.book1),
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
-                modifier = Modifier.aspectRatio(1f)
+                modifier = Modifier.fillMaxSize()
             )
 
         }
 
-        Text(text = author)
-        Text(text = bookName)
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(top = 10.dp),
+            text = bookData.author,
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center,
+            color = Color.Gray
+        )
+
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(top = 10.dp),
+            text = bookData.name,
+            fontSize = 24.sp,
+            color = Color.Black
+        )
 
     }
 
@@ -45,6 +96,20 @@ fun BookItem(
 @Composable
 fun BookItemPreview() {
     BookAppTheme {
-        BookItem(R.drawable.book1, "Akrom Malik", "Halqa")
+        BookItem(
+            BookData(
+                1,
+                "Halqa",
+                "Ajoyib",
+                "",
+                "",
+                "Akrom Malik",
+                1,
+                "",
+                1,
+                1,
+                1
+            )
+        )
     }
 }
